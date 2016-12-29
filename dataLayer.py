b8c9c1ccb71cf5167d95f2e7919786a48a23fc75
ThreeSixes <actual@joshlucy.com>
2016-12-29 15:21:32 -0800

    X

--- dataLayer.py
+++ dataLayer.py
@@ -44,7 +44,7 @@ class dataLayer:
         if config.redisSettings['enabled']:
             try:
                 # Build Redis object
-                self.__r.set(config.redisSettings['htLastName'], json.dumps({'samples': records}), ex = config.redisSettings['htLastExpire'])
+                self.__r.set(config.redisSettings['htLastName'], json.dumps(records), ex = config.redisSettings['htLastExpire'])
             
             except:
                 print(traceback.format_exc())
