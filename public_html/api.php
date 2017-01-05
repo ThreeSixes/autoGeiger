8d031107d9c98ece07eb62abecbcc457e4c3dab5
ThreeSixes <actual@joshlucy.com>
2017-01-04 16:10:12 -0800

    Ooops.

--- public_html/api.php
+++ public_html/api.php
@@ -2,8 +2,8 @@
 // API request router.
 
 // Do we have anything in $_GET?
-if (len($_GET) > 0) {
-    $x = explode($_GET, '/');
+if (len($_GET['t']) > 0) {
+    $x = explode($_GET['t'], '/');
     print_r($x);
 } else {
     print("Route -> API root.");
