974ec465bdbe2db7eaf941040b9f98612e2fb914
ThreeSixes <actual@joshlucy.com>
2017-01-04 21:48:01 -0800

    Fix mistakes. Many of them!

--- public_html/index.php
+++ public_html/index.php
@@ -6,22 +6,20 @@
         <H3>PUBROW background radiation experiment.</H3>
         <BR />
         <BR />
-        <H5>
-            Dafuhq is PUBROW?
-            <B>P</B>ortland
-            <BR />
-            <B>U</B>rban
-            <BR />
-            <B>B</B>ackground
-            <BR />
-            <B>R</B>adiation
-            <BR />
-            <B>O</B>observeration
-            <BR />
-            <B>W</B>orker
-            <BR />
-            <BR />
-            Source available on <A HREF="https://github.com/ThreeSixes/autoGeiger">GitHub</A>
-        </H5>
+        Dafuhq is PUBROW?
+        <B>P</B>ortland
+        <BR />
+        <B>U</B>rban
+        <BR />
+        <B>B</B>ackground
+        <BR />
+        <B>R</B>adiation
+        <BR />
+        <B>O</B>observeration
+        <BR />
+        <B>W</B>orker
+        <BR />
+        <BR />
+        Source available on <A HREF="https://github.com/ThreeSixes/autoGeiger">GitHub</A>
     </BODY>
 </HTML>
