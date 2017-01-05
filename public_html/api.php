73bf87afd47bb9260665fe1192d41742ba686257
ThreeSixes <actual@joshlucy.com>
2017-01-04 16:16:08 -0800

    X

--- public_html/api.php
+++ public_html/api.php
@@ -1,7 +1,7 @@
 <?php
 // Do we have anything in $_GET?
 if (count($_GET['t']) > 1) {
-    $x = explode($_GET['t'], '/');
+    $x = explode($_GET['t'], "/");
     print_r($x);
 } else {
     print("Route -> API root.");
