ae364c32db730617f0d59054d220aa8352d3c987
ThreeSixes <actual@joshlucy.com>
2017-01-04 21:46:30 -0800

    Text change.

--- public_html/index.php
+++ public_html/index.php
@@ -3,9 +3,25 @@
         <TITLE>PUBROW</TITLE>
     </HEAD>
     <BODY>
-        PUBROW background radiation experiment.
+        <H3>PUBROW background radiation experiment.</H3>
         <BR />
         <BR />
-        Source available on <A HREF="https://github.com/ThreeSixes/autoGeiger">GitHub</A>
+        <H5>
+            Dafuhq is PUBROW?
+            <B>P</B>ortland
+            <BR />
+            <B>U</B>rban
+            <BR />
+            <B>B</B>ackground
+            <BR />
+            <B>R</B>adiation
+            <BR />
+            <B>O</B>observeration
+            <BR />
+            <B>W</B>orker
+            <BR />
+            <BR />
+            Source available on <A HREF="https://github.com/ThreeSixes/autoGeiger">GitHub</A>
+        </H5>
     </BODY>
 </HTML>
