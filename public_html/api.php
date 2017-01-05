e91c6d384da3313e38de769f96c3bd409742396a
ThreeSixes <actual@joshlucy.com>
2017-01-04 16:12:20 -0800

    Arrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrgh....

--- public_html/api.php
+++ public_html/api.php
@@ -1,8 +1,9 @@
 <?php
 // API request router.
-
+print(count($_GET) . "\n");
+print(count($_GET['t']) . "\n");
 // Do we have anything in $_GET?
-if (len($_GET['t']) > 0) {
+if (count($_GET['t']) > 0) {
     $x = explode($_GET['t'], '/');
     print_r($x);
 } else {
