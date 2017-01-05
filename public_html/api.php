746898d97f203af23469448cc154588afeebade5
ThreeSixes <actual@joshlucy.com>
2017-01-04 16:25:20 -0800

    Prototype router functionality.

--- public_html/api.php
+++ public_html/api.php
@@ -1,13 +1,27 @@
 <?php
-
-if (isset($_GET['t'])) {
-    print("I can haz t!\n");
-}
-// Do we have anything in $_GET?
-if (strlen($_GET['t']) >= 1) {
-    $x = explode($_GET['t'], "/");
-    print_r($x);
-} else {
+// Show the root of /api.
+function showRoot() {
+    // This is where we'd display the data we want to display when someone hits the root of API (hasn't sent a command).
     print("Route -> API root.");
 }
+
+/*
+ * Request router
+ */
+
+// Do we have a path specified?
+if (isset($_GET['t'])) {
+    // Do we have anything in $_GET?
+    if (strlen($_GET['t']) >= 1) {
+        $x = explode("/", $_GET['t']);
+        print_r($x);
+    } else {
+        // Show the root.
+        showRoot();
+    }
+} else {
+    // Show the root.
+    showRoot();
+}
+
 ?>
