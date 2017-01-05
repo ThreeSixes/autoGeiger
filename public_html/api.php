e8daf8c34fb26174a7a3303de835053f66d1f73e
ThreeSixes <actual@joshlucy.com>
2017-01-04 16:09:12 -0800

    API request router test.

--- public_html/api.php
+++ public_html/api.php
@@ -1,3 +1,11 @@
 <?php
-print_r($_GET);
+// API request router.
+
+// Do we have anything in $_GET?
+if (len($_GET) > 0) {
+    $x = explode($_GET, '/');
+    print_r($x);
+} else {
+    print("Route -> API root.");
+}
 ?>
