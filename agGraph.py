7271b91dd52f80f6a6e44734f2688acdbed6010e
ThreeSixes <actual@joshlucy.com>
2017-01-09 18:46:17 -0800

    Change stacking order of environmental data.

--- agGraph.py
+++ agGraph.py
@@ -81,15 +81,14 @@ class agGraph:
 		# Generic elements for environmental readings.
 		self.__enviroGen = [
 			"--vertical-label", "kPa + %rH",
-			"DEF:bp=%s:baroPres:LAST" %config.graphSettings['enviroRRDPath'],
-			"DEF:bt=%s:baroTemp:LAST" %config.graphSettings['enviroRRDPath'],
 			"DEF:hr=%s:humidRh:LAST" %config.graphSettings['enviroRRDPath'],
 			"DEF:ht=%s:humidTemp:LAST" %config.graphSettings['enviroRRDPath'],
-			#"VDEF:kPa=baroPres,100,/",
-			"LINE1:bp#FF00FF:Barometric pressure",
-			"LINE1:bt#FFFF00:Barometer temp.",
+			"DEF:bp=%s:baroPres:LAST" %config.graphSettings['enviroRRDPath'],
+			"DEF:bt=%s:baroTemp:LAST" %config.graphSettings['enviroRRDPath'],
 			"LINE1:hr#00FF00:Humidity",
-			"LINE1:ht#FF0000:Hygrometer temp."
+			"LINE1:ht#FF0000:Hygrometer temp.",
+			"LINE1:bp#FF00FF:Barometric pressure",
+			"LINE1:bt#FFFF00:Barometer temp."
 		]
 		
 		# Graph parameters.
