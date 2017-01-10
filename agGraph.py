2402009ce0772cb1845ce726664dd4a139a6bf62
ThreeSixes <actual@joshlucy.com>
2017-01-09 20:08:01 -0800

    Another attempt at thigns.

--- agGraph.py
+++ agGraph.py
@@ -74,7 +74,7 @@ class agGraph:
 			"--vertical-label", "Counts/sec",
 			"DEF:cps=%s:cps:LAST" %config.graphSettings['geigerRRDPath'],
 			"DEF:alarm=%s:alarm:LAST" %config.graphSettings['geigerRRDPath'],
-			"GPRINT:\"%6lf\"",
+			"GPRINT:cps:LAST:\"%6.2lf%sW\"",
 			"LINE1:cps#00FFFF:Counts/sec",
 			"LINE1:alarm#FF0000:GC alarm"
 		]
